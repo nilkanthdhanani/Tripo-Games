a6bbbb45c54b5ae363b9772ef8ec60716f153e3b
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2024-12-10 16:20:36 +0530

    mobile view homepage

--- src/assets/images/svg/downArrow.js
+++ src/assets/images/svg/downArrow.js
@@ -1,5 +1,5 @@
 const DownArrow = () => (
-    <svg fill="#000000" width="20px" height="20px" viewBox="0 0 24 24" id="down-arrow" xmlns="http://www.w3.org/2000/svg" class="icon line"><path id="primary" d="M12,3V21M9,18l3,3,3-3" style={{ fill: "none", stroke: "#000", strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: "1.5" }}></path></svg>
+    <svg fill="#000000" width="20px" height="20px" viewBox="0 0 24 24" id="down-arrow" xmlns="http://www.w3.org/2000/svg" className="icon line"><path id="primary" d="M12,3V21M9,18l3,3,3-3" style={{ fill: "none", stroke: "#000", strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: "1.5" }}></path></svg>
 );
 
 export default DownArrow;
